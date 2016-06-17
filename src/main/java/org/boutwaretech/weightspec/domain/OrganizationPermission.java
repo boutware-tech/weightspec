c5221253648e8eca29d69c5c02e4c0506b92038a
Jeff Rutt <jeff.btpro@gmail.com>
2016-06-16 20:09:52 -0500

    Added OrganizationPermission entity and repository

---
+++ src/main/java/org/boutwaretech/weightspec/domain/OrganizationPermission.java
@@ -0,0 +1,57 @@
+package org.boutwaretech.weightspec.domain;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class OrganizationPermission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String organization;
+    
+    private List<String> permissions;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public List<String> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<String> permissions) {
+        this.permissions = permissions;
+    }
+}
---
+++ src/main/java/org/boutwaretech/weightspec/repositories/OrganizationPermissionRepository.java
@@ -0,0 +1,8 @@
+package org.boutwaretech.weightspec.repositories;
+
+import org.boutwaretech.weightspec.domain.OrganizationPermission;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface OrganizationPermissionRepository extends PagingAndSortingRepository<OrganizationPermission, Integer> {
+
+}
