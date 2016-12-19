2dc267dec710b071199cdbb8369df86edb9217b4
ruttjk <jeff.boutware@gmail.com>
2016-12-18 20:11:41 -0600

    Upgrade spring-boot-starter-parent to 1.4.2.RELEASE

--- src/test/java/org/boutwaretech/weightspec/WeightspecApplicationTests.java
+++ src/test/java/org/boutwaretech/weightspec/WeightspecApplicationTests.java
@@ -3,11 +3,12 @@ package org.boutwaretech.weightspec;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.boutwaretech.weightspec.WeightspecApplication;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = WeightspecApplication.class)
+@RunWith(SpringRunner.class)
 @WebAppConfiguration
 public class WeightspecApplicationTests {
 
